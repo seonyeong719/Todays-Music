e3d5e1c9c73092492d94b1b126059023a7febcae
seonyeong719 <sy904280@gmail.com>
2023-07-04 01:44:16 +0900

    :sparkles:Feat: 폰트 추가, 폴더 형태 변결

--- todays-music/src/App.tsx
+++ todays-music/src/App.tsx
@@ -1,6 +1,8 @@
 import { theme } from "./Style/theme";
 import GlobalStyles from "./Style/global";
 import { ThemeProvider } from "styled-components";
+import { RouterProvider } from "react-router-dom";
+import router from "Routes/routing";
 
 function App() {
   // const queryClient = new QueryClient();
@@ -8,7 +10,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      {/* // <RouterProvider router={router} /> */}
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
--- todays-music/src/Style/global.ts
+++ todays-music/src/Style/global.ts
@@ -4,27 +4,19 @@ import reset from "styled-reset";
 const GlobalStyles = createGlobalStyle`
     /* ${reset} */
     *{
-        box-sizing: border-box;
-        /* @font-face {
-		font-family: 'Nanum_light';
-		src: url('/Assets/Font/NanumSquareNeo-aLt.ttf');
+		box-sizing: border-box;
+        @font-face {
+		font-family: 'LINESeedKR-Th';
+		src: url('/Asset/Font/LINESeedKR-Th.ttf');
 	    }
         @font-face {
-		font-family: 'Nanum_regular';
-		src: url('/Assets/Font/NanumSquareNeo-bRg.ttf');
+		font-family: 'LINESeedKR-Rg';
+		src: url('/Asset/Font/LINESeedKR-Rg.ttf');
 	    }
         @font-face {
-		font-family: 'Nanum_bold';
-		src: url('/Assets/Font/NanumSquareNeo-cBd.ttf');
+		font-family:'LINESeedKR-Bd';
+		src: url('/Asset/Font/LINESeedKR-Bd.ttf');
 	    }
-        @font-face {
-		font-family: 'Nanum_extraBold';
-		src: url('/Assets/Font/NanumSquareNeo-dEb.ttf');
-	    }
-        @font-face {
-		font-family: 'Nanum_heavy';
-		src: url('/Assets/Font/NanumSquareNeo-eHv.ttf');
-	    } */
     }
 	body{
 		font-family: 'Nanum_regular';
