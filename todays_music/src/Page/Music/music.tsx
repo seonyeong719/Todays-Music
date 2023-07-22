6d0d9dbffa6ce84ac08ce96de1ac84d658af8885
seonyeong719 <sy904280@gmail.com>
2023-07-23 00:54:29 +0900

    :sparkles: Feat: 음악 mockData 추가

--- todays_music/src/Page/Music/music.tsx
+++ todays_music/src/Page/Music/music.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { flexAllCenter, flexJustifyCenter } from "../../Style/common";
-import { SUNNY_MUSIC } from "../../Consts/musicList";
+import { CLOUDY_MUSIC, RAINY_MUSIC, SNOW_MUSIC } from "../../Consts/musicList";
 
 function Music() {
   return (
@@ -10,11 +10,11 @@ function Music() {
         <S.MusicContainer>
           <img src="https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/8uEC/image/LkDCPehk0dm5Rz3m19-Y3DIC6u4" />
           <S.MusicList>
-            <h3>노래제목</h3>
-            <div>가수명</div>
-            {SUNNY_MUSIC.map((list) => (
+            {SNOW_MUSIC.map((list) => (
               <S.Li>
-                <div>{list.title}</div>
+                <div>
+                  {list.title} - {list.singer}
+                </div>
                 <audio src={list.audio} controls></audio>
               </S.Li>
             ))}
@@ -27,13 +27,15 @@ function Music() {
 export default Music;
 
 const Wrapper = styled.div`
-  background: linear-gradient(#d1c0a8, #e8ded1); // #fafdf3
+  background: linear-gradient(#d1c0a8, #e8ded1, #fafdf3);
   width: 100%;
   height: 100vh;
   ${flexAllCenter}
 `;
+
 const MusicWrapper = styled.div`
   width: 60%;
+  padding-top: 2rem;
 `;
 
 const Title = styled.span`
@@ -41,7 +43,7 @@ const Title = styled.span`
   color: white;
   font-family: "Lobster-Regular.ttf";
   ${flexJustifyCenter}
-  margin-top: 10rem;
+  margin-top: 6rem;
   padding-bottom: 2rem;
   border-bottom: 1px solid white;
 `;
@@ -50,15 +52,14 @@ const MusicContainer = styled.div`
   display: flex;
   margin: 70px 50px 0 50px;
   & > img {
-    /* width: 35%; */
     min-width: 25rem;
     max-height: 25rem;
     margin-right: 100px;
   }
 `;
+
 const MusicList = styled.div`
   text-align: start;
-  color: white;
   width: 100%;
   & > h3 {
     font-size: 40px;
@@ -71,7 +72,11 @@ const MusicList = styled.div`
   }
 `;
 const Li = styled.div`
+  & > div {
+    padding: 0.5rem 0;
+  }
   & > audio {
+    width: 27rem;
   }
 `;
 const S = {
--- todays_music/src/Utils/weatherData.ts
+++ todays_music/src/Utils/weatherData.ts
@@ -21,3 +21,27 @@ export const WeatherData = (sky: string, pty: string) => {
       return "/Assets/Img/구름.png";
   }
 };
+
+export const MusicData = (sky: string, pty: string) => {
+  switch (pty) {
+    case "0":
+      console.log("none");
+      break;
+    case "1":
+      return "/Assets/Img/비.png";
+    case "2":
+      return "/Assets/Img/눈.png";
+    case "3":
+      return "/Assets/Img/눈.png";
+    case "4":
+      return "/Assets/Img/비.png";
+  }
+  switch (sky) {
+    case "1":
+      return "/Assets/Img/햇빛.png";
+    case "3":
+      return "/Assets/Img/햇빛,구름.png";
+    case "4":
+      return "/Assets/Img/구름.png";
+  }
+};
