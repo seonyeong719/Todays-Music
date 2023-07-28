4a963b842f40d188054983e86d0cf39abe82b250
seonyeong719 <sy904280@gmail.com>
2023-07-28 17:24:10 +0900

    :lipstick: Design: 반응형 css 완료

--- todays_music/src/Components/Layout/Header/mobileHeader.tsx
+++ todays_music/src/Components/Layout/Header/mobileHeader.tsx
@@ -1,28 +1,53 @@
 import { RxHamburgerMenu } from "react-icons/rx";
 import { styled } from "styled-components";
 import { flexAlignCenter } from "../../../Style/common";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { navMenu } from "../../../Consts/navMenuList";
 
 function MobileHeader() {
   const [open, setOpen] = useState<boolean>(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    setOpen(false);
+  }, [location]);
 
   const onSideBarBtn = () => {
     setOpen((prev) => !prev);
   };
+
+  const onClickNav = (page: string) => {
+    navigate(`${page}`);
+  };
+
   return (
     <S.Wrapper>
       <S.Wrap>
         <S.Title>Sunny Sounds</S.Title>
       </S.Wrap>
       <S.Hamburger onClick={onSideBarBtn} />
+      {open && (
+        <S.Div>
+          {navMenu.map((nav, idx) => (
+            <S.Li key={idx} onClick={() => onClickNav(nav.address)}>
+              {nav.title}
+            </S.Li>
+          ))}
+        </S.Div>
+      )}
     </S.Wrapper>
   );
 }
 export default MobileHeader;
 
 const Wrapper = styled.div`
+  position: relative;
   ${flexAlignCenter}
   display: none;
+  width: 100%;
+  justify-content: space-between;
   @media ${({ theme }) => theme.device.mobile} {
     display: block;
     ${flexAlignCenter};
@@ -42,7 +67,7 @@ const LogoImg = styled.img`
 const Title = styled.span`
   font-size: 2.5rem;
   font-family: "LINESeedKR-Bd";
-  color: ${({ theme }) => theme.COLOR.subColor}; // 색깔 추후 수정 예정
+  color: white;
 `;
 
 const Hamburger = styled(RxHamburgerMenu)`
@@ -51,4 +76,23 @@ const Hamburger = styled(RxHamburgerMenu)`
   cursor: pointer;
 `;
 
-const S = { Wrapper, Wrap, LogoImg, Title, Hamburger };
+const Div = styled.div`
+  position: absolute;
+  background-color: beige;
+  right: 0;
+  top: 4rem;
+  font-size: 2rem;
+  height: 15rem;
+  width: 11rem;
+  padding: 1rem 0 0 0.8rem;
+  opacity: 0.7;
+`;
+
+const Li = styled.li`
+  color: white;
+  list-style: none;
+  padding-top: 0.7rem;
+  color: black;
+`;
+
+const S = { Wrapper, Wrap, LogoImg, Title, Hamburger, Li, Div };
